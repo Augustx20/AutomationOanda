89a93026a7f875dc39182ce1ad29f0647c1e194b
Augusto Machado <augus1726@gmail.com>
2023-03-20 14:36:43 -0300

    Update Excel

---
+++ Components/Excel.js
@@ -0,0 +1,42 @@
+const xlsx = require("xlsx")
+const Oanda = require('../ExchangeRate/Oanda');
+
+const SaveExcel = async () => {
+
+    setTimeout(() => {
+      //  Oanda
+      let EURUSD = Oanda.OandaArray[0];
+      let EURCOP = Oanda.OandaArray[1];
+      let CNYUSD = Oanda.OandaArray[2];
+      let JPYUSD = Oanda.OandaArray[3];
+      const wb = xlsx.readFile("Datos.xlsx");
+  
+  const datos = [
+      ["","Bancos","","","","Oanda"],
+      ["Country","To /From","Amount","","","To/From","Amount"],
+      ["Costa Rica","USD CRC","","","","EUR USD",EURUSD],
+      ["Uruguay","USD UYU","","","","EUR COP",EURCOP],
+      ["Colombia","USD COP","","","","CNY USD",CNYUSD],
+      ["Peru","USD PEN","","","","JPY USD",JPYUSD],
+      ["Peru","EUR PEN","","","","CNY COP",""],
+      ["Chile","USD CLP","","","","JPY COP",""],
+      ["Chile","EUR CLP","","","","BRL USD",""],
+      ["Guatemala","USD GTQ","","","","KRW USD",""],
+      ["Honduras","USD HNL","","","","USD CLP",""],
+    ];
+  
+    var ws = xlsx.utils.aoa_to_sheet(datos);
+  
+    wb.Sheets['Datos'] = ws;
+  
+    xlsx.writeFile(wb, "Datos.xlsx"); 
+    
+    console.log("Excel Actualizado")
+   
+  }, 6500)
+  
+  }
+
+  module.exports = {
+      SaveExcel
+  }
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const ProcessOanda = require('./ExchangeRate/Oanda')
-
+const ProcessExcel = require('./Components/Excel')
 ProcessOanda.Oanda();
+setTimeout(() => { 
+    ProcessExcel.SaveExcel()
+    }, 15000); 
 
